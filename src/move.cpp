d82b0e63a60cac77b00bba3ee7933de69cc75465
Rasim Sadıkoğlu <sadikoglurasim@gmail.com>
2022-12-07 01:32:21 +0300

    small fix

--- src/move.cpp
+++ src/move.cpp
@@ -162,15 +162,17 @@ uint16_t HeuristicMove::calculate_heuristic_score(std::bitset<33> board) {
             right = translate_1d(j + 1 < 7 ? i * 7 + j + 1 : -1),
             bottom = translate_1d(i + 1 < 7 ? i * 7 + j + 7 : -1);
 
-        uint8_t loneliness = 1;
-        loneliness += !((left == 0xff) && board[left]);
+        // uint8_t loneliness = 1;
+        uint8_t loneliness = 0;
+        loneliness += !((left != 0xff) && board[left]);
         loneliness += !((top != 0xff) && board[top]);
         loneliness += !((right != 0xff) && board[right]);
         loneliness += !((bottom != 0xff) && board[bottom]);
 
-        uint8_t distance = std::abs((int8_t)i - 3) + std::abs((int8_t)j - 3);
+        // uint8_t distance = std::abs((int8_t)i - 3) + std::abs((int8_t)j - 3);
 
-        score += loneliness * distance;
+        // score += loneliness * distance;
+        score += loneliness;
     }
     return score;
 }
