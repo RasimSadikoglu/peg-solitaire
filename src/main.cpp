4c898e07c382338f7ee4881d5babb072f7f5d9a0
Rasim Sadıkoğlu <sadikoglurasim@gmail.com>
2022-12-10 16:58:29 +0300

    macro fix

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,11 @@ int main(int argc, char* argv[]) {
         std::make_pair("dfs-heuristic", peg_solitaire::depth_first_search_heuristic_selection),
     };
 
-#ifndef BYPASS_TIME_MEMORY_LIMIT
     peg_solitaire::set_time_limit(std::atoi(argv[2]));
     std::thread helper_thread(peg_solitaire::helper_thread);
-#endif
 
     peg_solitaire::set_algorithm(argv[1]);
     jump_table[argv[1]]();
 
-#ifndef BYPASS_TIME_MEMORY_LIMIT
     helper_thread.join();
-#endif
 }
